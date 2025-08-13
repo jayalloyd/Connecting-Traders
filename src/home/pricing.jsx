55fc8c5a608986f41f5963180e6ffc5f94390c69
jayarani.ys <jayaraniys1@gmail.com>
2025-08-13 21:47:00 +0530

    stats section in home page imoplemented

--- src/home/pricing.jsx
+++ src/home/pricing.jsx
@@ -1,7 +1,32 @@
 import React from 'react';
 
 function Pricing() {
-    return (<h1>pricing</h1>  );
+    return (<div className='container'>
+  <div className="row">
+    <div className="col-4">
+        <h1 className='fs-4 mb-3' style={{ textAlign: 'left' }}>Unbeatable pricing
+</h1>
+<p className="text-muted" style={{ textAlign: 'start' }}>We pioneered the concept of discount broking and price transparency in India. Flat fees and no hidden charges.</p>
+    <a href="">See pricing &rarr;</a>
+
+    </div>
+    <div className="col-2"></div>
+    <div className="col-6">
+        <div className="row text-center">
+            <div className="col border p-2">
+                <h1 className='mb-3'>&#8377;0</h1>
+                <p className="text-muted">Free account
+opening</p>
+            </div>
+
+            <div className="col p-2 border"><h1 className='mb-3'>&#8377;20</h1><p className="text-muted">Intraday F & O</p></div>
+        </div>
+    </div>
+    
+  </div>
+
+
+    </div>  );
 }
 
 export default Pricing;
--- src/home/stats.jsx
+++ src/home/stats.jsx
@@ -1,6 +1,37 @@
 import React from 'react';
 function Stats() {
-    return ( <h1>stats</h1> );
+    return (<div className="container p-5">
+        <div className="row p-5">
+
+            <div className="col-6 p-5">
+                <h1 className='fs-3 p-5' style={{ textAlign: 'left' }}>Trust with confidence</h1>
+                <h2 className='fs-4' style={{ textAlign: 'left' }}>Customer-first always</h2>
+                <p className="text-muted" style={{ textAlign: 'start' }}>That's why 1.6+ crore customers trust connecting-traders with ~ ₹6 lakh crores of equity investments, making us India’s largest broker; contributing to 15% of daily retail exchange volumes in India.</p>
+
+                <h2 className='fs-4' style={{ textAlign: 'left' }}>No spam or gimmicks
+                </h2>
+                <p className="text-muted" style={{ textAlign: 'start' }}>No gimmicks, spam, "gamification", or annoying push notifications. High quality apps that you use at your pace, the way you like. Our philosophies.</p>
+                <h2 className='fs-4' style={{ textAlign: 'left' }}>The Traders universe</h2>
+                <p className="text-muted" style={{ textAlign: 'start' }}>Not just an app, but a whole ecosystem. Our investments in 30+ fintech startups offer you tailored services specific to your needs.</p>
+                <h2 className='fs-4' style={{ textAlign: 'left' }}>No spam or gimmicks
+                </h2>
+                <p className="text-muted" style={{ textAlign: 'start' }}>No gimmicks, spam, "gamification", or annoying push notifications. High quality apps that you use at your pace, the way you like. Our philosophies.</p>
+                <h2 className='fs-4' style={{ textAlign: 'left' }}>Do better with money
+                </h2>
+                <p className="text-muted" style={{ textAlign: 'start' }}>With initiatives like Nudge and Kill Switch, we don't just facilitate transactions, but actively help you do better with your money.</p>
+
+            </div>
+            <div className="col-6 p-5">
+                <img src="./media/ecosystem.png" style={{ width: "150%" }} />
+                <div >
+                    <a href="">Explore our products &rarr;</a>&nbsp;&nbsp;&nbsp;
+                    <a href="">Try kite demo &rarr;</a>
+
+                </div>
+            </div>
+        </div>
+    </div>
+    );
 }
 
 export default Stats;
